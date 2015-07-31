1462f444ed618adbace11f35daf4aaf28a77d4a1
Max <contact@mstoiber.com>
2015-07-31 10:23:11 +0200

    Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,12 @@ module.exports = function(grunt) {
 		            annotation: 'css/maps' // ...to the specified directory
 		        },
 		        processors: [
-		           	require('postcss-import')({
+		           	require('postcss-import')({ // import all the css files into main.css
 		           		from: "css/main.css"
 		           	}),
-		           	require('postcss-simple-vars')(),
+		           	require('postcss-simple-vars')(), // replace the variables
 		           	require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
-		           	require('cssnano')(), // minify the result
+		           	require('cssnano')(), // and minify the result
 		        ]
 	       	},
 	       	dist: {
@@ -23,15 +23,15 @@ module.exports = function(grunt) {
 	    },
 	    watch: {
 	      	css: {
-	      		files: ['css/*.css', 'css/*/*.css'],
-	        	tasks: ['css'],
+	      		files: ['css/*.css', 'css/*/*.css'], // if any of the .css files change
+	        	tasks: ['postcss'], // run the css task
 	        	options: {
 	        		spawn: false
 	        	},
 	      	},
 	      	scripts: {
-	      		files: ['js/*.js', 'js/*/*.js'],
-	      		tasks: ['browserify', 'uglify'],
+	      		files: ['js/*.js', 'js/*/*.js'], // if any of the .js files change
+	      		tasks: ['browserify', 'uglify'], // transform all the requires into js/bundle.js and then uglify js/bundle.js into js/bundle.min.js
 	      		options: {
 	      			spawn: false
 	      		}
@@ -50,7 +50,7 @@ module.exports = function(grunt) {
 		    	dest: 'js/bundle.js',
 		    },
 	    	options: {
-	    		transform: ['reactify']
+	    		transform: ['reactify'] // transform jsx into js
 	    	}
 	    },
 	    uglify: {
@@ -68,7 +68,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-babel');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	// Default task(s).
-	grunt.registerTask('css', ['postcss']);
+	// Default task
 	grunt.registerTask('default', ['connect', 'watch']);
 };
