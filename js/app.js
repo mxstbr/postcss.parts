3ee47742f48b15d41a928798e0fc418e61858b12
Jason Brown <jbrown@miradorfin.com>
2016-09-03 10:41:39 -0700

    Add scroll management so things go to top on change

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { Router, Route, browserHistory, IndexRoute } from "react-router";
+import { applyRouterMiddleware, Router, Route, browserHistory, IndexRoute } from "react-router";
+import { useScroll } from 'react-router-scroll';
+
 import App from "./components/App.react";
 import TagList from "./components/TagList.react";
 import NotFound from "./components/NotFound";
@@ -19,7 +21,7 @@ import Tags from "./tags";
 const history = syncHistoryWithStore(browserHistory, store);
 
 var routes = (
-      <Router history={history}>
+      <Router history={history} render={applyRouterMiddleware(useScroll())}>
         <Route path="/" component={App}>
             <IndexRoute component={TagList} />
             {
