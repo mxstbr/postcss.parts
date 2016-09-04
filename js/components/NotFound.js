735e93190bd8af403ea70dba089f28fed2e6b9bc
Max <contact@mstoiber.com>
2016-09-04 17:19:31 +0200

    Convert NotFound

--- js/components/NotFound.js
+++ js/components/NotFound.js
@@ -1,24 +1,27 @@
 import React from "react";
+import styled from 'styled-components';
 import capitalizeFirstLetter from "../utils/capitalize";
 import getTag from "../utils/url";
 
-const NotFound = ({location}) => {
+const Heading = styled.h2`
+text-align: center;
+padding: 5em 0;
+`;
 
-    const { pathname } = location;
+const NotFound = ({ location }) => {
+  const { pathname } = location;
 
-    let tagText = '';
-    const tag = getTag(pathname);
+  let tagText = '';
+  const tag = getTag(pathname);
+  if (tag) {
+    tagText = `Tag ${capitalizeFirstLetter(tag)} cannot be found. `
+  }
 
-    if (tag) {
-        tagText = `Tag ${capitalizeFirstLetter(tag)} cannot be found. `
-    }
-
-
-    return (
-        <div>
-            <h2 className="notfound">{tagText}Try searching for a PostCSS Plugin.</h2>
-        </div>
-    )
+  return (
+    <div>
+      <Heading>{tagText}Try searching for a PostCSS Plugin.</Heading>
+    </div>
+  )
 }
 
 export default NotFound;
