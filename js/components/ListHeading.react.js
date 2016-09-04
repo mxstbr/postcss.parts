9bd945b9c01752fe3d8fd4a8029dd593817100c5
Max <contact@mstoiber.com>
2016-09-04 17:17:37 +0200

    Convert ListHeading

--- js/components/ListHeading.react.js
+++ js/components/ListHeading.react.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { Link } from "react-router";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+padding: 1em;
+margin: 0;
+border-bottom: 1px solid $light-grey;
+text-align: center;
+width: 100%;
+position: relative;
+> h2 {
+  padding: 0;
+	margin: 0;
+}
+`;
+
+const Icon = styled.svg`
+height: 1.5em;
+width: 1.5em;
+`;
 
 const ListHeading = ({text}) => {
     return(
-      <div className="search__heading">
+      <Wrapper>
         <Link to="/" className="search__heading-back">
-            <svg width="24" height="24" viewBox="0 0 24 24">
+            <Icon width="24" height="24" viewBox="0 0 24 24">
               <g stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" fill="none">
                 <path d="M.5.5h10v10h-10zM13.5.5h10v10h-10zM.5 13.5h10v10h-10zM13.5 13.5h10v10h-10z"/>
               </g>
-            </svg>
+            </Icon>
         </Link>
         <h2>{text}</h2>
-      </div>
+      </Wrapper>
     );
 }
 
