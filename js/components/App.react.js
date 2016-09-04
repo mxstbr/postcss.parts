50fc5e6a3edfe542e00a0fd9480fde1930ad224d
Max <contact@mstoiber.com>
2016-09-04 15:37:22 +0200

    Convert App wrapper

--- js/components/App.react.js
+++ js/components/App.react.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import withRouter from "react-router/lib/withRouter";
+import styled from 'styled-components';
+import constants from '../constants';
 import capitalizeFirstLetter from "../utils/capitalize";
 
 import Header from './Header.react';
@@ -11,48 +13,65 @@ import { loadPlugins } from "../actions/Plugins";
 import { connect } from "react-redux";
 import getTag from "../utils/url";
 
+const Wrapper = styled.section`
+  max-width: ${constants.maxWidth};
+  margin: 0 auto;
+  display: block;
+  border-radius: 5px;
+  background-color: white;
+  margin-bottom: 2em;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);
+  &:after {
+    content: "";
+    display: block;
+    height: 0;
+    clear: both;
+  }
+`;
+
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
 
-    constructor(props, context) {
-        super(props, context);
-        this.handleSearch = this.handleSearch.bind(this);
-    }
+  componentDidMount() {
+    this.props.loadPlugins();
+  }
 
-    componentDidMount() {
-        this.props.loadPlugins();   
-    }
+  handleSearch(evt) {
+    const { location: { pathname , query: { searchTerm }} } = this.props;
+    //Create one history slot for having a query so the back button will remove the query
+    const action = searchTerm ? 'replace' : 'push';
+    this.props.router[action](`${pathname}?searchTerm=${evt.target.value}`)
+	}
 
 	render() {
-        const { children, location: { query: { searchTerm }, pathname } } = this.props;
-        const tag = getTag(pathname);
-        let content = children;
+    const { children, location: { query: { searchTerm }, pathname } } = this.props;
+    const tag = getTag(pathname);
+    let content = children;
 
-        if (!tag && searchTerm) {
-            content = <PluginList {...this.props} />
-        }
+    if (!tag && searchTerm) {
+      content = <PluginList {...this.props} />
+    }
 
 		return(
 			<div>
 				<Header />
-				<section className="wrapper">
+				<Wrapper>
 					<SearchField value={searchTerm || ""} tag={tag} onChange={this.handleSearch} />
-                    { tag ? <ListHeading key="ListHeading" text={capitalizeFirstLetter(tag)}></ListHeading> : null }
+          {tag && (
+            <ListHeading key="ListHeading" text={capitalizeFirstLetter(tag)}></ListHeading>
+          )}
 					{ content }
-				</section>
+				</Wrapper>
 			</div>
 		);
-	}
-
-	handleSearch(evt) {
-        const { location: { pathname , query: { searchTerm }} } = this.props;
-        //Create one history slot for having a query so the back button will remove the query
-        const action = searchTerm ? 'replace' : 'push';
-        this.props.router[action](`${pathname}?searchTerm=${evt.target.value}`)
 	}
 }
 
 const mapDispatchToProps = {
-    loadPlugins
-}
+  loadPlugins
+};
 
 export default connect(undefined, mapDispatchToProps)(withRouter(App));
