23802e86cf40788cd452c840073294cb53a93b41
Max <contact@mstoiber.com>
2016-09-04 15:45:12 +0200

    Convert Header

--- js/components/Header.react.js
+++ js/components/Header.react.js
@@ -1,18 +1,60 @@
 import React from "react";
 import Link from "react-router/lib/Link";
+import styled from 'styled-components';
+
+import constants from '../constants';
+
+const HeaderWrapper = styled.section`
+  width: 100%;
+  text-align: center;
+  margin: 5em 0;
+`;
+
+const Title = styled.h1`
+  font-size: 3em;
+  color: ${constants.brandColor};
+  margin-bottom: 0.25em;
+`;
+
+const Subtitle = styled.h2`
+  font-weight: 400;
+  color: ${constants.lightBrandColor};
+  margin-top: 0;
+`;
+
+const TopLink = styled.a`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 2%;
+  color: ${constants.midGrey};
+  text-decoration: none;
+`;
+
+const LogoWrapper = styled.a`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 2%;
+  display: block;
+  font-size: 1.5em;
+  font-family: Georgia, Times, 'Times New Roman', serif;
+  text-decoration: none;
+  color: #999999;
+`;
 
 const Header = () => {
     return(
-			<section className="header">
-				<a href="https://twitter.com/mxstbr" className="header__logo" >
+			<HeaderWrapper>
+				<LogoWrapper href="https://twitter.com/mxstbr">
 					&lt;mxstbr/&gt;
-				</a>
-				<a href="https://github.com/himynameisdave/postcss-plugins#submitting-a-new-plugin" className="header__add">Add a plugin</a>
-				<h1 className="header__title">
+				</LogoWrapper>
+				<TopLink href="https://github.com/himynameisdave/postcss-plugins#submitting-a-new-plugin">Add a plugin</TopLink>
+				<Title>
 					<Link to="/" className="header__link">PostCSS.parts</Link>
-				</h1>
-				<h2 className="header__subtitle">A searchable catalog of PostCSS plugins</h2>
-			</section>
+				</Title>
+				<Subtitle>A searchable catalog of PostCSS plugins</Subtitle>
+			</HeaderWrapper>
     );
 }
 
--- js/constants.js
+++ js/constants.js
@@ -3,6 +3,8 @@ const colors = {
 	midGrey: '#999',
 	textColor: '#222',
 	maxWidth: '968px',
+	brandColor: '#087B98',
+	lightBrandColor: '#3C93A9',
 };
 
 export default colors;
