9facc36f7eda5a98b1208576e78163cd8cec05d1
Max <contact@mstoiber.com>
2016-09-10 09:45:52 +0200

    Convert SearchField component

--- js/components/SearchField.react.js
+++ js/components/SearchField.react.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
 import capitalizeFirstLetter from "../utils/capitalize";
+import constants from "../constants";
 
 import Clear from "./Clear.react";
+
+const SearchWrapper = styled.div`
+  width: 100%;
+  border-bottom: 1px solid ${constants.lightGrey};
+  position: relative;
+  padding-right: 2em;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 2em;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  border: none;
+  font-size: 1.5em;
+  line-height: 1.5em;
+  padding-left: 1em;
+  border-radius: 5px 5px 0 0;
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+  outline: none;
+`;
 
 class SearchField extends Component {
   render() {
@@ -9,26 +35,25 @@ class SearchField extends Component {
     const { tag, value, onChange } = this.props;
 
     let placeholder = "Search";
-    
+
     if (tag) {
       placeholder += " in " + capitalizeFirstLetter(tag);
     }
 
     const location = tag ? `/tag/${tag}` : '/';
-    
+
     return (
-      <div className="plugin__search">
-        <input 
-          className="plugin__search-field" 
-          value={value} 
-          type="text" 
-          placeholder={placeholder} 
-          autoFocus 
+      <SearchWrapper>
+        <SearchInput
+          value={value}
+          type="text"
+          placeholder={placeholder}
+          autoFocus
           onChange={onChange}
           ref={(c) => this.input = c}
         />
         { value && <Clear location={location} onClear={() => this.input.focus()} /> }
-      </div>
+      </SearchWrapper>
     );
   }
 }
