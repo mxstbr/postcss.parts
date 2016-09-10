b16590b3a11ec02ae3943aefe0c070ea029705a5
Max <contact@mstoiber.com>
2016-09-10 09:58:44 +0200

    Convert TagListItem

--- js/components/TagList.react.js
+++ js/components/TagList.react.js
@@ -4,11 +4,11 @@ import TagListItem from "./TagListItem.react";
 import Tags from "../tags";
 
 const TagList = ({ selectTag }) => {
-    return(
-        <div>
-            {Tags.map((tag) => <TagListItem key={"tag-" + tag} name={tag} />)}
-        </div>
-    )
+  return(
+    <div>
+      {Tags.map((tag) => <TagListItem key={"tag-" + tag} name={tag} />)}
+    </div>
+  );
 }
 
 export default TagList;
--- js/components/TagListItem.react.js
+++ js/components/TagListItem.react.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { Link } from "react-router";
+import styled from "styled-components";
+
 import capitalizeFirstLetter from "../utils/capitalize";
+import constants from "../constants";
+
+const TagLink = styled(Link)`
+width: 25%;
+float: left;
+height: calc(${constants.maxWidth} / 4);
+text-align: center;
+display: flex;
+align-items: center;
+justify-content: center;
+border-bottom: 1px solid ${constants.lightGrey};
+border-right: 1px solid ${constants.lightGrey};
+text-decoration: none;
+color: #000;
+
+&:hover {
+  background-color: ${constants.lightGrey};
+  cursor: pointer;
+}
+
+&:nth-child(4n) {
+  border-right: none;
+}
+
+@media (max-width: ${constants.maxWidth}) {
+  width: 50%;
+  height: calc(${constants.maxWidth} / 4);
+}
+`;
 
 const TagListItem = ({name}) => {
-    return(
-        <Link to={`/tag/${name}`} className="tag">
-            <h2 className="tag__name">{capitalizeFirstLetter(name)}</h2>
-        </Link>
-    );
+  return(
+    <TagLink to={`/tag/${name}`}>
+      <h2>{capitalizeFirstLetter(name)}</h2>
+    </TagLink>
+  );
 }
 
 export default TagListItem;
