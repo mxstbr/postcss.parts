bdee74e49613e4ab726182d90993c79d3d4e82c5
Max <contact@mstoiber.com>
2016-09-10 09:49:30 +0200

    Convert Clear

--- js/components/Clear.react.js
+++ js/components/Clear.react.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Link from "react-router/lib/Link";
+import styled from "styled-components";
 
-const Close = ({ location, onClear }) => {
+import constants from "../constants";
+
+const ClearLink = styled(Link)`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  font-size: 1em;
+  color: ${constants.textColor};
+  text-decoration: none;
+  padding: 1em;
+  border-left: 1px solid ${constants.lightGrey};
+
+  &:hover {
+    background-color: ${constants.lightGrey};
+  }
+`;
+
+const Clear = ({ location, onClear }) => {
   return (
-      <Link to={location} onClick={onClear} className="plugin__search__clear-link">X</Link>
+    <ClearLink to={location} onClick={onClear}>X</ClearLink>
   )
 }
 
-export default Close;
+export default Clear;
